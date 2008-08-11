7779a2633e4ceb7ee9c3a4a596290b9db08edfad
Jason Torres <jason.e.torres@gmail.com>
2008-08-11 08:03:39 +0800

    Checkin all the other required files

---
+++ sinatra_activerecord.rb
@@ -0,0 +1,17 @@
+require 'yaml'
+require 'activerecord'
+require 'erb'
+
+ENV['APP_ENV'] ||= 'development'
+ENV['APP_ROOT'] ||= "#{__FILE__}/../"
+
+APP_ENV = ENV['APP_ENV']
+APP_ROOT = ENV['APP_ROOT']
+
+def database_configuration_file
+  File.join(File.dirname(__FILE__), 'config', 'database.yml')
+end
+
+def database_configuration
+  YAML::load(ERB.new(IO.read(database_configuration_file)).result)
+end
